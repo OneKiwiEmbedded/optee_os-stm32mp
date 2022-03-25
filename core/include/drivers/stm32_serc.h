184a2578ac95b6f53a2175be82575d72a55ca7e5
Ludovic Barre <ludovic.barre@foss.st.com>
2022-03-25 17:23:18 +0100

    drivers: firewall: add stm32 serc support
    Add stm32 SERC driver support.
    
    Change-Id: I14bfa4c08c3eccd7f4dde54bc79536a71a296ac5
    Signed-off-by: Ludovic Barre <ludovic.barre@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/245732
    Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
    Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>

---
+++ core/include/drivers/stm32_serc.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2020-2024, STMicroelectronics
+ */
+#ifndef __DRIVERS_STM32_SERC_H__
+#define __DRIVERS_STM32_SERC_H__
+
+#include <types_ext.h>
+
+struct serc_driver_data {
+	uint32_t version;
+	uint8_t num_ilac;
+};
+
+struct stm32_serc_platdata {
+	uintptr_t base;
+	struct clk *clock;
+	int irq;
+};
+
+/*
+ * Platform can define an access violation action to trap cpu/reset core or system
+ * after an eventual debug trace.
+ */
+void access_violation_action(void);
+
+#endif /* __DRIVERS_STM32_SERC_H__ */
