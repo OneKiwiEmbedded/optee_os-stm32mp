7c732c7125999650af6e47700f88b511e6f3bb53
Etienne Carriere <etienne.carriere@foss.st.com>
2023-08-22 18:33:59 +0200

    product/optee-stm32mp1: sync config with product optee-stm32mp2
    Product optee-stm32mp1 gets the SCP-firmware configuration data from
    OP-TEE using strcut scpfw_config format. Function scpfw_configure() is
    called during SCP-firmware initialization to generate the tables SCP
    firmware expect from the struct scpfw_config passed by OP-TEE.
    
    With this change, there is no more static configuration on SCP-firmware
    for optee-stm32mp1. The config data are fully provided by OP-TEE core.
    
    Change-Id: I84fda5883968b12081598a16fac186d81838d6fc
    Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/SCP-firmware/+/323864
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/377927
    Tested-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
    Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
    Domain-Review: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

--- core/lib/scmi-server/SCP-firmware/product/optee-stm32mp1/fw/Firmware.cmake
+++ core/lib/scmi-server/SCP-firmware/product/optee-stm32mp1/fw/Firmware.cmake
@@ -31,15 +31,15 @@ set(SCP_ENABLE_SCMI_RESET_INIT TRUE)
 
 set(SCP_ENABLE_IPO_INIT FALSE)
 
-list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/stm32_pmic_regu")
-list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/stm32_pwr_regu")
+list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/stm32_regu_consumer")
+list(PREPEND SCP_MODULE_PATHS "${CMAKE_CURRENT_LIST_DIR}/../module/psu_optee_regulator")
 
 # The order of the modules in the following list is the order in which the
 # modules are initialized, bound, started during the pre-runtime phase.
 # any change in the order will cause firmware initialization errors.
 
 list(APPEND SCP_MODULES "optee-mbx")
-list(APPEND SCP_MODULES "optee-smt")
+list(APPEND SCP_MODULES "msg-smt")
 list(APPEND SCP_MODULES "scmi")
 list(APPEND SCP_MODULES "optee-clock")
 list(APPEND SCP_MODULES "clock")
@@ -47,8 +47,13 @@ list(APPEND SCP_MODULES "scmi-clock")
 list(APPEND SCP_MODULES "optee-reset")
 list(APPEND SCP_MODULES "reset-domain")
 list(APPEND SCP_MODULES "scmi-reset-domain")
-list(APPEND SCP_MODULES "stm32-pmic-regu")
-list(APPEND SCP_MODULES "stm32-pwr-regu")
+list(APPEND SCP_MODULES "stm32-regu-consumer")
 list(APPEND SCP_MODULES "voltage-domain")
 list(APPEND SCP_MODULES "scmi-voltage-domain")
+if (CFG_STM32MP13)
+    list(APPEND SCP_MODULES "psu-optee-regulator")
+    list(APPEND SCP_MODULES "psu")
+    list(APPEND SCP_MODULES "dvfs")
+    list(APPEND SCP_MODULES "scmi-perf")
+endif (CFG_STM32MP13)
 list(APPEND SCP_MODULES "optee-console")
