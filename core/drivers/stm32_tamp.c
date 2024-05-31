4ms/optee-stm32mp25

drivers: stm32_tamp: allow subnode to be probed
TAMP peripheral includes a NVMEM block. It will be exposed using NVMEM
framework as a subnode of tamp. Therefore we need to parse all the
children of a "stm32-tamp" compatible node

Change-Id: I57d0ef71d742754c7cfe9bce4b230cd26975d677
Signed-off-by: Simeon Marijon <simeon.marijon@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/328923
Reviewed-by: Simeon MARIJON <simeon.marijon@st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Tested-by: Simeon MARIJON <simeon.marijon@st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>

core/drivers/stm32_tamp.c
@@ -1548,13 +1548,14 @@ static int stm32_tamp_parse_active_conf(const void *fdt, int node,
 	return 0;
 }
 
-static
-TEE_Result stm32_tamp_parse_fdt(struct stm32_tamp_platdata *pdata,
-				const void *fdt, int node, const void *compat)
+static TEE_Result stm32_tamp_parse_fdt(struct stm32_tamp_platdata *pdata,
+				       const void *fdt, int node,
+				       const void *compat)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
-	int pinnode = -1;
-	struct dt_node_info dt_tamp = { };
+	int pinnode = -FDT_ERR_NOTFOUND;
+	int subnode = -FDT_ERR_NOTFOUND;
+	struct dt_node_info dt_tamp = {};
 	bool __unused is_tdcid = false;
 
 	_fdt_fill_device_info(fdt, &dt_tamp, node);
@@ -1586,10 +1587,21 @@ TEE_Result stm32_tamp_parse_fdt(struct stm32_tamp_platdata *pdata,
 	stm32_tamp_parse_passive_conf(fdt, node, pdata);
 	stm32_tamp_parse_active_conf(fdt, node, pdata);
 
-	fdt_for_each_subnode(pinnode, fdt, node) {
-		res = stm32_tamp_configure_pin_from_dt(fdt, pinnode, pdata);
-		if (res)
-			return res;
+	fdt_for_each_subnode(subnode, fdt, node) {
+		if (fdt_getprop(fdt, node, "pinctrl-0", NULL)) {
+			res = stm32_tamp_configure_pin_from_dt(fdt, subnode,
+							       pdata);
+			if (res)
+				return res;
+			pinnode = subnode;
+		}
+
+		res = dt_driver_maybe_add_probe_node(fdt, subnode);
+		if (res) {
+			EMSG("Failed on node %s with %#" PRIx32,
+			     fdt_get_name(fdt, subnode, NULL), res);
+			panic();
+		}
 	}
 
 	if (pinnode < 0 && pinnode != -FDT_ERR_NOTFOUND)

OneKiwiEmbedded/optee_os-stm32mp

core/drivers/stm32_tamp.c
@@ -1546,13 +1546,14 @@ static int stm32_tamp_parse_active_conf(const void *fdt, int node,
 	return 0;
 }
 
-static
-TEE_Result stm32_tamp_parse_fdt(struct stm32_tamp_platdata *pdata,
-				const void *fdt, int node, const void *compat)
+static TEE_Result stm32_tamp_parse_fdt(struct stm32_tamp_platdata *pdata,
+				       const void *fdt, int node,
+				       const void *compat)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
-	int pinnode = -1;
-	struct dt_node_info dt_tamp = { };
+	int pinnode = -FDT_ERR_NOTFOUND;
+	int subnode = -FDT_ERR_NOTFOUND;
+	struct dt_node_info dt_tamp = {};
 	bool is_tdcid = false;
 
 	_fdt_fill_device_info(fdt, &dt_tamp, node);
@@ -1584,10 +1585,21 @@ TEE_Result stm32_tamp_parse_fdt(struct stm32_tamp_platdata *pdata,
 	stm32_tamp_parse_passive_conf(fdt, node, pdata);
 	stm32_tamp_parse_active_conf(fdt, node, pdata);
 
-	fdt_for_each_subnode(pinnode, fdt, node) {
-		res = stm32_tamp_configure_pin_from_dt(fdt, pinnode, pdata);
-		if (res)
-			return res;
+	fdt_for_each_subnode(subnode, fdt, node) {
+		if (fdt_getprop(fdt, subnode, "pinctrl-0", NULL)) {
+			res = stm32_tamp_configure_pin_from_dt(fdt, subnode,
+							       pdata);
+			if (res)
+				return res;
+			pinnode = subnode;
+		}
+
+		res = dt_driver_maybe_add_probe_node(fdt, subnode);
+		if (res) {
+			EMSG("Failed on node %s with %#" PRIx32,
+			     fdt_get_name(fdt, subnode, NULL), res);
+			panic();
+		}
 	}
 
 	if (pinnode < 0 && pinnode != -FDT_ERR_NOTFOUND)