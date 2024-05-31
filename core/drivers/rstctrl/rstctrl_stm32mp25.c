4ms/optee-stm32mp25

drivers: rstctrl: stm32mp25: exclude all RIF aware reset lines
Ignore all reset lines from RIF aware IPs.

Change-Id: Id044d038b9dcc235633a407e5f7cce3077385931
Signed-off-by: Gabriel Fernandez <gabriel.fernandez@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/247283
Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>

core/drivers/rstctrl/rstctrl_stm32mp25.c
@@ -25,8 +25,51 @@ static struct rstctrl_ops stm32_rstctrl_setr_without_assert_ops = {
 	.deassert_level = reset_deassert_setr,
 };
 
+static TEE_Result reset_assert_do_nothing(struct rstctrl *rstctrl __unused,
+					  unsigned int to_us __unused)
+{
+	return TEE_SUCCESS;
+}
+
+static TEE_Result reset_deassert_do_nothing(struct rstctrl *rstctrl __unused,
+					    unsigned int to_us __unused)
+{
+	return TEE_SUCCESS;
+}
+
+static struct rstctrl_ops stm32_rstctrl_do_nothing_ops = {
+	.assert_level = reset_assert_do_nothing,
+	.deassert_level = reset_deassert_do_nothing,
+};
+
+static unsigned int rif_aware_reset[] = {
+	DDRCP_R, DDRCAPB_R, DDRPHYCAPB_R, DDRCFG_R, DDR_R,
+	IPCC1_R, IPCC2_R,
+	HPDMA1_R, HPDMA2_R, HPDMA2_R, LPDMA_R,
+	GPIOA_R, GPIOB_R, GPIOC_R, GPIOD_R,
+	GPIOE_R, GPIOF_R, GPIOG_R, GPIOH_R,
+	GPIOI_R, GPIOJ_R, GPIOK_R, GPIOZ_R,
+	HSEM_R,
+	FMC_R,
+};
+
+static bool stm32_rstctrl_is_rif_aware_ip(unsigned int id)
+{
+	size_t i = 0;
+
+	for (i = 0; i < ARRAY_SIZE(rif_aware_reset); i++) {
+		if (id == rif_aware_reset[i])
+			return true;
+	}
+
+	return false;
+}
+
 static struct rstctrl_ops *stm32_reset_mp25_find_ops(unsigned int id)
 {
+	if (stm32_rstctrl_is_rif_aware_ip(id))
+		return &stm32_rstctrl_do_nothing_ops;
+
 	switch (id) {
 	case SYS_R:
 	case C1_R:

OneKiwiEmbedded/optee_os-stm32mp

core/drivers/rstctrl/rstctrl_stm32mp25.c
@@ -25,8 +25,51 @@ static struct rstctrl_ops stm32_rstctrl_setr_without_assert_ops = {
 	.deassert_level = reset_deassert_setr,
 };
 
+static TEE_Result reset_assert_do_nothing(struct rstctrl *rstctrl __unused,
+					  unsigned int to_us __unused)
+{
+	return TEE_SUCCESS;
+}
+
+static TEE_Result reset_deassert_do_nothing(struct rstctrl *rstctrl __unused,
+					    unsigned int to_us __unused)
+{
+	return TEE_SUCCESS;
+}
+
+static struct rstctrl_ops stm32_rstctrl_do_nothing_ops = {
+	.assert_level = reset_assert_do_nothing,
+	.deassert_level = reset_deassert_do_nothing,
+};
+
+static unsigned int rif_aware_reset[] = {
+	DDRCP_R, DDRCAPB_R, DDRPHYCAPB_R, DDRCFG_R, DDR_R,
+	IPCC1_R, IPCC2_R,
+	HPDMA1_R, HPDMA2_R, HPDMA3_R, LPDMA_R,
+	GPIOA_R, GPIOB_R, GPIOC_R, GPIOD_R,
+	GPIOE_R, GPIOF_R, GPIOG_R, GPIOH_R,
+	GPIOI_R, GPIOJ_R, GPIOK_R, GPIOZ_R,
+	HSEM_R,
+	FMC_R,
+};
+
+static bool stm32_rstctrl_is_rif_aware_ip(unsigned int id)
+{
+	size_t i = 0;
+
+	for (i = 0; i < ARRAY_SIZE(rif_aware_reset); i++) {
+		if (id == rif_aware_reset[i])
+			return true;
+	}
+
+	return false;
+}
+
 static struct rstctrl_ops *stm32_reset_mp25_find_ops(unsigned int id)
 {
+	if (stm32_rstctrl_is_rif_aware_ip(id))
+		return &stm32_rstctrl_do_nothing_ops;
+
 	switch (id) {
 	case SYS_R:
 	case C1_R: