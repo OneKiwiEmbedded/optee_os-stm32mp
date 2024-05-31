OP-TEE/optee_os

drivers: crypto: stm32_saes: fallback to software on 192bit AES keys
Implement AES software operation for 192 bits keys as these are not
supported by the STM32 SAES peripheral. For that purpose ciphering final,
context copy and context freeing operations common functions are split
into CRYP/SAES peripheral specific functions.

Reviewed-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>
Signed-off-by: Etienne Carriere <etienne.carriere@foss.st.com>

core/drivers/crypto/stm32/cipher.c
@@ -27,6 +27,9 @@ struct cryp_ctx {
 struct saes_ctx {
 	struct stm32_saes_context ctx;
 	enum stm32_saes_chaining_mode algo;
+	/* Fallback to software implementation on 192bit AES key */
+	bool use_fallback;
+	struct crypto_cipher_ctx *fallback_ctx;
 };
 
 /*
@@ -47,6 +50,8 @@ struct ip_cipher_ops {
 			   const uint8_t *iv, size_t iv_len);
 	TEE_Result (*update)(union ip_ctx *ctx, bool last_block, uint8_t *src,
 			     uint8_t *dst, size_t len);
+	void (*final)(union ip_ctx *ctx);
+	void (*copy_state)(union ip_ctx *dst_ctx, union ip_ctx *src_ctx);
 };
 
 struct stm32_cipher_ctx {
@@ -87,9 +92,17 @@ static TEE_Result cryp_update(union ip_ctx *ip_ctx, bool last_block,
 	return stm32_cryp_update(&ip_ctx->cryp.ctx, last_block, src, dst, len);
 }
 
+static void cryp_copy_state(union ip_ctx *dst_ip_ctx, union ip_ctx *src_ip_ctx)
+{
+	assert(IS_ENABLED(CFG_STM32_CRYP));
+
+	memcpy(&dst_ip_ctx->cryp, &src_ip_ctx->cryp, sizeof(dst_ip_ctx->cryp));
+}
+
 static const struct ip_cipher_ops cryp_ops = {
 	.init = cryp_init,
 	.update = cryp_update,
+	.copy_state = cryp_copy_state,
 };
 
 static TEE_Result saes_init(union ip_ctx *ip_ctx, bool is_decrypt,
@@ -101,6 +114,34 @@ static TEE_Result saes_init(union ip_ctx *ip_ctx, bool is_decrypt,
 	if (!IS_ENABLED(CFG_STM32_SAES))
 		return TEE_ERROR_NOT_IMPLEMENTED;
 
+	if (key_len == AES_KEYSIZE_192) {
+		struct crypto_cipher_ctx *ctx = ip_ctx->saes.fallback_ctx;
+		TEE_OperationMode mode = TEE_MODE_ILLEGAL_VALUE;
+		TEE_Result res = TEE_ERROR_GENERIC;
+
+		if (!IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK)) {
+			EMSG("STM32 SAES does not support 192bit keys");
+
+			return TEE_ERROR_NOT_IMPLEMENTED;
+		}
+
+		if (is_decrypt)
+			mode = TEE_MODE_DECRYPT;
+		else
+			mode = TEE_MODE_ENCRYPT;
+
+		res = ctx->ops->init(ctx, mode, key, key_len, NULL, 0, iv,
+				     iv_len);
+		if (res)
+			return res;
+
+		ip_ctx->saes.use_fallback = true;
+
+		return TEE_SUCCESS;
+	}
+
+	ip_ctx->saes.use_fallback = false;
+
 	return stm32_saes_init(&ip_ctx->saes.ctx, is_decrypt, ip_ctx->saes.algo,
 			       key_sel, key, key_len, iv, iv_len);
 }
@@ -111,12 +152,52 @@ static TEE_Result saes_update(union ip_ctx *ip_ctx, bool last_block,
 	if (!IS_ENABLED(CFG_STM32_SAES))
 		return TEE_ERROR_NOT_IMPLEMENTED;
 
+	if (ip_ctx->saes.use_fallback) {
+		struct crypto_cipher_ctx *ctx = ip_ctx->saes.fallback_ctx;
+
+		assert(IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK));
+
+		return ctx->ops->update(ctx, last_block, src, len, dst);
+	}
+
 	return stm32_saes_update(&ip_ctx->saes.ctx, last_block, src, dst, len);
+}
+
+static void saes_final(union ip_ctx *ip_ctx)
+{
+	struct crypto_cipher_ctx *ctx = ip_ctx->saes.fallback_ctx;
+
+	assert(IS_ENABLED(CFG_STM32_SAES));
+
+	if (ip_ctx->saes.use_fallback) {
+		assert(IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK));
+		ctx->ops->final(ctx);
+	}
+}
+
+static void saes_copy_state(union ip_ctx *dst_ip_ctx, union ip_ctx *src_ip_ctx)
+{
+	struct saes_ctx *src_ctx = &src_ip_ctx->saes;
+	struct crypto_cipher_ctx *fb_ctx = src_ctx->fallback_ctx;
+
+	assert(IS_ENABLED(CFG_STM32_SAES));
+
+	memcpy(&dst_ip_ctx->saes.ctx, &src_ctx->ctx, sizeof(src_ctx->ctx));
+
+	dst_ip_ctx->saes.algo = src_ctx->algo;
+	dst_ip_ctx->saes.use_fallback = src_ctx->use_fallback;
+
+	if (src_ctx->use_fallback) {
+		assert(IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK));
+		fb_ctx->ops->copy_state(dst_ip_ctx->saes.fallback_ctx, fb_ctx);
+	}
 }
 
 static const struct ip_cipher_ops saes_ops = {
 	.init = saes_init,
 	.update = saes_update,
+	.final = saes_final,
+	.copy_state = saes_copy_state,
 };
 
 static struct stm32_cipher_ctx *
@@ -147,14 +228,18 @@ static TEE_Result stm32_cipher_update(struct drvcrypt_cipher_update *dupdate)
 
 static void stm32_cipher_final(void *ctx __unused)
 {
+	struct stm32_cipher_ctx *c = to_stm32_cipher_ctx(ctx);
+
+	if (c->ops->final)
+		c->ops->final(&c->ip_ctx);
 }
 
 static void stm32_cipher_copy_state(void *dst_ctx, void *src_ctx)
 {
-	struct stm32_cipher_ctx *src = to_stm32_cipher_ctx(src_ctx);
-	struct stm32_cipher_ctx *dst = to_stm32_cipher_ctx(dst_ctx);
+	struct stm32_cipher_ctx *src_c = to_stm32_cipher_ctx(src_ctx);
+	struct stm32_cipher_ctx *dst_c = to_stm32_cipher_ctx(dst_ctx);
 
-	memcpy(dst, src, sizeof(*dst));
+	src_c->ops->copy_state(&dst_c->ip_ctx, &src_c->ip_ctx);
 }
 
 static TEE_Result alloc_cryp_ctx(void **ctx, enum stm32_cryp_algo_mode algo)
@@ -197,48 +282,75 @@ static TEE_Result stm32_cryp_cipher_allocate(void **ctx, uint32_t algo)
 	}
 }
 
-static TEE_Result alloc_saes_ctx(void **ctx, enum stm32_saes_chaining_mode algo)
-{
-	struct stm32_cipher_ctx *c = calloc(1, sizeof(*c));
-
-	if (!c)
-		return TEE_ERROR_OUT_OF_MEMORY;
-
-	FMSG("Using SAES %d", algo);
-	c->ip_ctx.saes.algo = algo;
-	c->ops = &saes_ops;
-	*ctx = &c->c_ctx;
-
-	return TEE_SUCCESS;
-}
-
-static TEE_Result stm32_saes_cipher_allocate(void **ctx, uint32_t algo)
-{
-	/*
-	 * Convert TEE_ALGO id to internal id
-	 */
-	switch (algo) {
-	case TEE_ALG_AES_ECB_NOPAD:
-		return alloc_saes_ctx(ctx, STM32_SAES_MODE_ECB);
-	case TEE_ALG_AES_CBC_NOPAD:
-		return alloc_saes_ctx(ctx, STM32_SAES_MODE_CBC);
-	case TEE_ALG_AES_CTR:
-		return alloc_saes_ctx(ctx, STM32_SAES_MODE_CTR);
-	default:
-		return TEE_ERROR_NOT_IMPLEMENTED;
-	}
-}
-
-static void stm32_cipher_free(void *ctx)
+static void stm32_cryp_cipher_free(void *ctx)
 {
 	struct stm32_cipher_ctx *c = to_stm32_cipher_ctx(ctx);
 
 	free(c);
 }
 
+static TEE_Result stm32_saes_cipher_allocate(void **ctx, uint32_t algo)
+{
+	enum stm32_saes_chaining_mode saes_algo = STM32_SAES_MODE_ECB;
+	struct crypto_cipher_ctx *fallback_ctx = NULL;
+	struct stm32_cipher_ctx *saes_ctx = NULL;
+	TEE_Result res = TEE_SUCCESS;
+
+	switch (algo) {
+	case TEE_ALG_AES_ECB_NOPAD:
+		saes_algo = STM32_SAES_MODE_ECB;
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			res = crypto_aes_ecb_alloc_ctx(&fallback_ctx);
+		break;
+	case TEE_ALG_AES_CBC_NOPAD:
+		saes_algo = STM32_SAES_MODE_CBC;
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			res = crypto_aes_cbc_alloc_ctx(&fallback_ctx);
+		break;
+	case TEE_ALG_AES_CTR:
+		saes_algo = STM32_SAES_MODE_CTR;
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			res = crypto_aes_ctr_alloc_ctx(&fallback_ctx);
+		break;
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
+	}
+	if (res)
+		return res;
+
+	saes_ctx = calloc(1, sizeof(*saes_ctx));
+	if (!saes_ctx) {
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			fallback_ctx->ops->free_ctx(fallback_ctx);
+
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+	FMSG("Using SAES %d", saes_algo);
+	saes_ctx->ip_ctx.saes.algo = saes_algo;
+	saes_ctx->ops = &saes_ops;
+	saes_ctx->ip_ctx.saes.fallback_ctx = fallback_ctx;
+	*ctx = &saes_ctx->c_ctx;
+
+	return TEE_SUCCESS;
+}
+
+static void stm32_saes_cipher_free(void *ctx)
+{
+	struct stm32_cipher_ctx *c = to_stm32_cipher_ctx(ctx);
+
+	if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK)) {
+		struct crypto_cipher_ctx *fb_ctx = c->ip_ctx.saes.fallback_ctx;
+
+		fb_ctx->ops->free_ctx(fb_ctx);
+	}
+
+	free(c);
+}
+
 static struct drvcrypt_cipher driver_cipher_cryp = {
 	.alloc_ctx = stm32_cryp_cipher_allocate,
-	.free_ctx = stm32_cipher_free,
+	.free_ctx = stm32_cryp_cipher_free,
 	.init = stm32_cipher_initialize,
 	.update = stm32_cipher_update,
 	.final = stm32_cipher_final,
@@ -247,7 +359,7 @@ static struct drvcrypt_cipher driver_cipher_cryp = {
 
 static struct drvcrypt_cipher driver_cipher_saes = {
 	.alloc_ctx = stm32_saes_cipher_allocate,
-	.free_ctx = stm32_cipher_free,
+	.free_ctx = stm32_saes_cipher_free,
 	.init = stm32_cipher_initialize,
 	.update = stm32_cipher_update,
 	.final = stm32_cipher_final,

OneKiwiEmbedded/optee_os-stm32mp

(cherry picked from commit 03de2c7bb316e08e74a3ee68f8ed132b204956b6)
Change-Id: I2f256282e2bc887114001a4427b54f885790290b
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/362623
Tested-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/drivers/crypto/stm32/cipher.c
@@ -28,6 +28,9 @@ struct cryp_ctx {
 struct saes_ctx {
 	struct stm32_saes_context ctx;
 	enum stm32_saes_chaining_mode algo;
+	/* Fallback to software implementation on 192bit AES key */
+	bool use_fallback;
+	struct crypto_cipher_ctx *fallback_ctx;
 };
 
 union ip_ctx {
@@ -41,6 +44,8 @@ struct ip_cipher_ops {
 			   const uint8_t *iv, size_t iv_len);
 	TEE_Result (*update)(union ip_ctx *ctx, bool last_block, uint8_t *src,
 			     uint8_t *dst, size_t len);
+	void (*final)(union ip_ctx *ctx);
+	void (*copy_state)(union ip_ctx *dst_ctx, union ip_ctx *src_ctx);
 };
 
 struct stm32_cipher_ctx {
@@ -87,9 +92,17 @@ static TEE_Result cryp_update(union ip_ctx *ip_ctx __unused,
 #endif
 }
 
+static void cryp_copy_state(union ip_ctx *dst_ip_ctx, union ip_ctx *src_ip_ctx)
+{
+	assert(IS_ENABLED(CFG_STM32_CRYP));
+
+	memcpy(&dst_ip_ctx->cryp, &src_ip_ctx->cryp, sizeof(dst_ip_ctx->cryp));
+}
+
 static const struct ip_cipher_ops cryp_ops = {
 	.init = cryp_init,
 	.update = cryp_update,
+	.copy_state = cryp_copy_state,
 };
 
 static TEE_Result saes_init(union ip_ctx *ip_ctx __unused,
@@ -100,6 +113,32 @@ static TEE_Result saes_init(union ip_ctx *ip_ctx __unused,
 {
 #ifdef CFG_STM32_SAES
 	enum stm32_saes_key_selection key_sel = STM32_SAES_KEY_SOFT;
+
+	if (key_len == AES_KEYSIZE_192) {
+		struct crypto_cipher_ctx *ctx = ip_ctx->saes.fallback_ctx;
+		TEE_OperationMode mode = TEE_MODE_ENCRYPT;
+		TEE_Result res = TEE_ERROR_GENERIC;
+
+		if (!IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK)) {
+			EMSG("STM32 SAES does not support 192bit keys");
+
+			return TEE_ERROR_NOT_IMPLEMENTED;
+		}
+
+		if (is_decrypt)
+			mode = TEE_MODE_DECRYPT;
+
+		res = ctx->ops->init(ctx, mode, key, key_len, NULL, 0, iv,
+				     iv_len);
+		if (res)
+			return res;
+
+		ip_ctx->saes.use_fallback = true;
+
+		return TEE_SUCCESS;
+	}
+
+	ip_ctx->saes.use_fallback = false;
 
 	return stm32_saes_init(&ip_ctx->saes.ctx, is_decrypt, ip_ctx->saes.algo,
 			       key_sel, key, key_len, iv, iv_len);
@@ -114,15 +153,55 @@ static TEE_Result saes_update(union ip_ctx *ip_ctx __unused,
 			      size_t len __unused)
 {
 #ifdef CFG_STM32_SAES
+	if (ip_ctx->saes.use_fallback) {
+		struct crypto_cipher_ctx *ctx = ip_ctx->saes.fallback_ctx;
+
+		assert(IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK));
+
+		return ctx->ops->update(ctx, last_block, src, len, dst);
+	}
+
 	return stm32_saes_update(&ip_ctx->saes.ctx, last_block, src, dst, len);
 #else
 	return TEE_ERROR_NOT_IMPLEMENTED;
 #endif
 }
 
+static void saes_final(union ip_ctx *ip_ctx)
+{
+	struct crypto_cipher_ctx *ctx = ip_ctx->saes.fallback_ctx;
+
+	assert(IS_ENABLED(CFG_STM32_SAES));
+
+	if (ip_ctx->saes.use_fallback) {
+		assert(IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK));
+		ctx->ops->final(ctx);
+	}
+}
+
+static void saes_copy_state(union ip_ctx *dst_ip_ctx, union ip_ctx *src_ip_ctx)
+{
+	struct saes_ctx *src_ctx = &src_ip_ctx->saes;
+	struct crypto_cipher_ctx *fb_ctx = src_ctx->fallback_ctx;
+
+	assert(IS_ENABLED(CFG_STM32_SAES));
+
+	memcpy(&dst_ip_ctx->saes.ctx, &src_ctx->ctx, sizeof(src_ctx->ctx));
+
+	dst_ip_ctx->saes.algo = src_ctx->algo;
+	dst_ip_ctx->saes.use_fallback = src_ctx->use_fallback;
+
+	if (src_ctx->use_fallback) {
+		assert(IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK));
+		fb_ctx->ops->copy_state(dst_ip_ctx->saes.fallback_ctx, fb_ctx);
+	}
+}
+
 static const struct ip_cipher_ops saes_ops = {
 	.init = saes_init,
 	.update = saes_update,
+	.final = saes_final,
+	.copy_state = saes_copy_state,
 };
 
 static struct stm32_cipher_ctx *
@@ -153,14 +232,18 @@ static TEE_Result stm32_cipher_update(struct drvcrypt_cipher_update *dupdate)
 
 static void stm32_cipher_final(void *ctx __unused)
 {
+	struct stm32_cipher_ctx *c = to_stm32_cipher_ctx(ctx);
+
+	if (c->ops->final)
+		c->ops->final(&c->ip_ctx);
 }
 
 static void stm32_cipher_copy_state(void *dst_ctx, void *src_ctx)
 {
-	struct stm32_cipher_ctx *src = to_stm32_cipher_ctx(src_ctx);
-	struct stm32_cipher_ctx *dst = to_stm32_cipher_ctx(dst_ctx);
+	struct stm32_cipher_ctx *src_c = to_stm32_cipher_ctx(src_ctx);
+	struct stm32_cipher_ctx *dst_c = to_stm32_cipher_ctx(dst_ctx);
 
-	memcpy(dst, src, sizeof(*dst));
+	src_c->ops->copy_state(&dst_c->ip_ctx, &src_c->ip_ctx);
 }
 
 static TEE_Result alloc_cryp_ctx(void **ctx, enum stm32_cryp_algo_mode algo)
@@ -203,48 +286,75 @@ static TEE_Result stm32_cryp_cipher_allocate(void **ctx, uint32_t algo)
 	}
 }
 
-static TEE_Result alloc_saes_ctx(void **ctx, enum stm32_saes_chaining_mode algo)
-{
-	struct stm32_cipher_ctx *c = calloc(1, sizeof(*c));
-
-	if (!c)
-		return TEE_ERROR_OUT_OF_MEMORY;
-
-	FMSG("Using SAES %d", algo);
-	c->ip_ctx.saes.algo = algo;
-	c->ops = &saes_ops;
-	*ctx = &c->c_ctx;
-
-	return TEE_SUCCESS;
-}
-
-static TEE_Result stm32_saes_cipher_allocate(void **ctx, uint32_t algo)
-{
-	/*
-	 * Convert TEE_ALGO id to internal id
-	 */
-	switch (algo) {
-	case TEE_ALG_AES_ECB_NOPAD:
-		return alloc_saes_ctx(ctx, STM32_SAES_MODE_ECB);
-	case TEE_ALG_AES_CBC_NOPAD:
-		return alloc_saes_ctx(ctx, STM32_SAES_MODE_CBC);
-	case TEE_ALG_AES_CTR:
-		return alloc_saes_ctx(ctx, STM32_SAES_MODE_CTR);
-	default:
-		return TEE_ERROR_NOT_IMPLEMENTED;
-	}
-}
-
-static void stm32_cipher_free(void *ctx)
+static void stm32_cryp_cipher_free(void *ctx)
 {
 	struct stm32_cipher_ctx *c = to_stm32_cipher_ctx(ctx);
 
 	free(c);
 }
 
+static TEE_Result stm32_saes_cipher_allocate(void **ctx, uint32_t algo)
+{
+	enum stm32_saes_chaining_mode saes_algo = STM32_SAES_MODE_ECB;
+	struct crypto_cipher_ctx *fallback_ctx = NULL;
+	struct stm32_cipher_ctx *saes_ctx = NULL;
+	TEE_Result res = TEE_SUCCESS;
+
+	switch (algo) {
+	case TEE_ALG_AES_ECB_NOPAD:
+		saes_algo = STM32_SAES_MODE_ECB;
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			res = crypto_aes_ecb_alloc_ctx(&fallback_ctx);
+		break;
+	case TEE_ALG_AES_CBC_NOPAD:
+		saes_algo = STM32_SAES_MODE_CBC;
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			res = crypto_aes_cbc_alloc_ctx(&fallback_ctx);
+		break;
+	case TEE_ALG_AES_CTR:
+		saes_algo = STM32_SAES_MODE_CTR;
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			res = crypto_aes_ctr_alloc_ctx(&fallback_ctx);
+		break;
+	default:
+		return TEE_ERROR_NOT_IMPLEMENTED;
+	}
+	if (res)
+		return res;
+
+	saes_ctx = calloc(1, sizeof(*saes_ctx));
+	if (!saes_ctx) {
+		if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK))
+			fallback_ctx->ops->free_ctx(fallback_ctx);
+
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+	FMSG("Using SAES %d", saes_algo);
+	saes_ctx->ip_ctx.saes.algo = saes_algo;
+	saes_ctx->ops = &saes_ops;
+	saes_ctx->ip_ctx.saes.fallback_ctx = fallback_ctx;
+	*ctx = &saes_ctx->c_ctx;
+
+	return TEE_SUCCESS;
+}
+
+static void stm32_saes_cipher_free(void *ctx)
+{
+	struct stm32_cipher_ctx *c = to_stm32_cipher_ctx(ctx);
+
+	if (IS_ENABLED(CFG_STM32_SAES_SW_FALLBACK)) {
+		struct crypto_cipher_ctx *fb_ctx = c->ip_ctx.saes.fallback_ctx;
+
+		fb_ctx->ops->free_ctx(fb_ctx);
+	}
+
+	free(c);
+}
+
 static struct drvcrypt_cipher driver_cipher_cryp = {
 	.alloc_ctx = stm32_cryp_cipher_allocate,
-	.free_ctx = stm32_cipher_free,
+	.free_ctx = stm32_cryp_cipher_free,
 	.init = stm32_cipher_initialize,
 	.update = stm32_cipher_update,
 	.final = stm32_cipher_final,
@@ -253,7 +363,7 @@ static struct drvcrypt_cipher driver_cipher_cryp = {
 
 static struct drvcrypt_cipher driver_cipher_saes = {
 	.alloc_ctx = stm32_saes_cipher_allocate,
-	.free_ctx = stm32_cipher_free,
+	.free_ctx = stm32_saes_cipher_free,
 	.init = stm32_cipher_initialize,
 	.update = stm32_cipher_update,
 	.final = stm32_cipher_final,